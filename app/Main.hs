08d92cc92f88bb7d5bbcb0cc263017ac450f6422
kaniehuest <77627612+kaniehuest@users.noreply.github.com>
2023-10-11 17:21:04 +0000

    Added tries

--- app/Main.hs
+++ app/Main.hs
@@ -2,14 +2,34 @@ import System.Random
 
 main :: IO ()
 main = do
-  randomNumber <- randomRIO (1 :: Int, 100 :: Int)
+  -- randomNumber <- randomRIO (1 :: Int, 100 :: Int)
+  let randomNumber = 5
 
-  putStrLn "Write a number betwen 1 and 100"
+  putStrLn "How many tries do you want?"
+  userTries <- getLine
+
+  putStrLn "Write a number betwen 1 and 100\n"
+  let userTriesNumber = (read userTries :: Int)
+  doComparisonNTimes userTriesNumber randomNumber
+
+
+doComparisonNTimes 0 randomNumber = do
+  putStrLn "Out of tries."
+  putStrLn $ "The number was: " ++ show randomNumber
+  return ()
+
+doComparisonNTimes n randomNumber =
+  do
   userTry <- getLine
-
-  let x = (read userTry :: Int)
-  putStrLn ("Your number was: " ++ userTry)
-  case compare x randomNumber of
-    EQ -> putStrLn "Your correctly guessed then number, congratulations!"
-    LT -> putStrLn $ "Your guess is smaller than the real number: " ++ show randomNumber
-    GT -> putStrLn $ "Your guess is greater than the real number: " ++ show randomNumber
+  let userTryNumber = (read userTry :: Int)
+  case compare userTryNumber randomNumber of
+    EQ -> do 
+      putStrLn "Your correctly guessed then number, congratulations!"
+      return ()
+    LT -> do
+      putStrLn "Your guess is smaller than the real number.\n"
+      doComparisonNTimes (n-1) randomNumber
+    GT -> do
+      putStrLn "Your guess is greater than the real number.\n"
+      doComparisonNTimes (n-1) randomNumber
+  
